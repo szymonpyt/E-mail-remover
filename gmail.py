02db9f648cc1033bb5de398345d77eb8ccd4464e
szymon_21 <szymon_21@wp.pl>
2022-01-13 17:20:10 +0100

    Adding argparse to put login/passw in cmd

--- gmail.py
+++ gmail.py
@@ -1,8 +1,19 @@
 import imaplib
+import argparse
+
+# add possibility to add args in terminal
+parser = argparse.ArgumentParser()
+parser.add_argument('--login', type=str, default="anymail@wp.com",
+                    help="What is your email address? ")
+parser.add_argument('--passw', type=str, default="p@ssword",
+                    help="What is password ?")
+parser.add_argument('--sub', type=str, default="Header",
+                    help="What is subject ?")
+args=parser.parse_args()
 
 # account cred
-email = "yourlogin@gmail.com"
-email_pass = "password"
+email = f"{args.login}"
+email_pass = f"{args.passw}"
 
 #initialize IMAP object for Gmail
 imap = imaplib.IMAP4_SSL("imap.gmail.com")
@@ -18,7 +29,7 @@ imap.login(email, email_pass)
 # select
 imap.select(mailbox='Inbox')
 
-status, message_id_list = imap.search(None, 'FROM', "address email or header")
+status, message_id_list = imap.search(None, 'FROM', f"{args.sub}")
 # return list of e-mails ids
 messages = message_id_list[0].split(b' ')
 
